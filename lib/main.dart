4c89aa1c0209573f5c8ab68cfbae926b3cf7f1f3
elsankry02 <elsankry02@gmail.com>
2026-06-03 07:54:55 +0300

    Integrate Firebase: add Firebase configuration files, initialize Firebase in main, and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'app/boot.dart';
-import 'core/databases/cache/cache_helper.dart';
+import 'package:dalel_app/app/app.dart';
+import 'package:dalel_app/app/boot.dart';
+import 'package:dalel_app/core/databases/cache/cache_helper.dart';
+import 'package:dalel_app/core/utils/constants/app_strings.dart';
+import 'package:dalel_app/firebase_options.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'app/app.dart';
-import 'core/utils/constants/app_strings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServiceLocator();
   await getIt<CacheHelper>().init();
-  await EasyLocalization.ensureInitialized();
   runApp(
     EasyLocalization(
       path: AppStrings.kTranslations,
