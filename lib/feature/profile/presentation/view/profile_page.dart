f0064c044178c60579f2a5c0439a15bbb9c0180b
elsankry02 <elsankry02@gmail.com>
2026-06-28 17:45:23 +0300

    fix: Adjust padding in ProfilePage and reorder import statements

--- lib/feature/profile/presentation/view/profile_page.dart
+++ lib/feature/profile/presentation/view/profile_page.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/functions/default_appbar.dart';
+import '../../../../core/themes/app_colors.dart';
 import '../../../../core/utils/constants/app_images.dart';
 import '../../../../core/utils/constants/app_svgs.dart';
 import '../../../../core/utils/extensions/extensions.dart';
-import '../../../../core/themes/app_colors.dart';
 import '../../../auth/data/auth_cubit/auth_cubit.dart';
 import '../../data/cubit/user_cubit.dart';
 import '../widget/default_list_tile_profile_widget.dart';
@@ -22,7 +22,7 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: defaultAppBar(context, title: "profile_title".tr()),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         physics: BouncingScrollPhysics(),
         children: [
           BlocBuilder<UserCubit, GetUserDataState>(
